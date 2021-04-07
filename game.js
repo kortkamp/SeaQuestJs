0adfecc1672254a03f6002f979f47ba45efdd097
kortkamp <marcelusmedius@gmail.com>
2021-04-07 16:53:40 -0300

    basic HUD implemented

--- game.js
+++ game.js
@@ -1,24 +1,4 @@
 
-// Atari 2600 pallete converted to RGB.
-// The index represents each of 128 NTSC collor  pallete.
-ntscTiaPallete = [
-	0x000000,0x404040,0x6c6c6c,0x909090,0xb0b0b0,0xc8c8c8,0xdcdcdc,0xececec,
-	0x444400,0x646410,0x848424,0xa0a034,0xb8b840,0xd0d050,0xe8e85c,0xfcfc68,
-	0x702800,0x844414,0x985c28,0xac783c,0xbc8c4c,0xcca05c,0xdcb468,0xecc878,
-	0x841800,0x983418,0xac5030,0xc06848,0xd0805c,0xe09470,0xeca880,0xfcbc94,
-	0x880000,0x9c2020,0xb03c3c,0xc05858,0xd07070,0xe08888,0xeca0a0,0xfcb4b4,
-	0x78005c,0x8c2074,0xa03c88,0xb0589c,0xc070b0,0xd084c0,0xdc9cd0,0xecb0e0,
-	0x480078,0x602090,0x783ca4,0x8c58b0,0xa070cc,0xb484dc,0xc49cec,0xd4b0fc,
-	0x140084,0x302098,0x4c3cac,0x6858c0,0x7c70d0,0x9488e0,0xa8a0ec,0xbcb4fc,
-	0x000088,0xac209c,0x3840b0,0x505cc0,0x6874d0,0x7c8ce0,0x90a4ec,0xa4b8fc,
-	0x00187c,0x1a3890,0x3854a8,0x5070bc,0x6888cc,0x7c9cdc,0x90b4ec,0xa4c8fc,
-	0x002c5c,0x1c4c78,0x386890,0x5084ac,0x689cc0,0x7cb4d4,0x90cce8,0xa4e0fc,
-	0x003c2c,0x1c5c48,0x387c64,0x509c80,0x68b494,0x7cd0ac,0x90e4c0,0xa4fcd4,
-	0x003c00,0x205c20,0x407c40,0x5c9c5c,0x74b474,0x8cd08c,0xa4e4a4,0xb8fcb8,
-	0x143800,0x345c1c,0x507c38,0x6c9850,0x84b468,0x9ccc7c,0xb4e490,0xc8fca4,
-	0x2c3000,0x4c501c,0x687034,0x848c4c,0x9ca864,0xb4c078,0xccd488,0xe0ec9c,
-	0x442800,0x644818,0x846830,0x4a8444,0xb89c58,0xd0b46c,0xe8cc7c,0xfce08c
-];
 
 var scanlines = 192;
 var canvasScale = 3;
@@ -26,40 +6,39 @@ var cv = document.getElementById("gameCanvas");
 var ctx = cv.getContext("2d");
 var score;
 var lifes;
-var x = 76;
-var y = 39;
-var vx = 0;
-var vy = 0;
+var oxygen;
+var rescuedDivers;
+var maxOxygenBar = 64;
+
 
 class Enemy {
 	constructor(sprite,x,y,color,hScale){
 		// Coord position XY
 		this.x = x;
 		this.y = y;
-		
 		// Speed X and Y
 		this.vx = 0;
 		this.vy = 0;
-		
-		this.collor = color;
+		this.hScale = 1;
+		this.color = color;
 		// Horizontal Scale
 		this.hScale = hScale;
 		// Default direction will be 1(left to right)
 		this.dir = 1;
 		// This sprite has 
 		this.moveLimit = true;
-		
 		// Haw many frames to draw a new sprite animation.
 		this.animationInterval = 4;
 		// Internal counter to do the animation.
 		this.animationCounter = 0;
+		// Animation speed, pow(animationSpeed,2) means how many frames to update sprite.
+		// Usually can be 2 or 3
+		this.animationSpeed = 2;
+	}
+	destroy(){
 		
 	}
-	update(){
-		this.y += this.vy;
-		this.x += this.vx;
-		
-				
+	checkLimits(){
 		if(this.x >= 134){
 			this.x = 134;
 		}
@@ -72,24 +51,27 @@ class Enemy {
 		if(this.y <= 39){
 			this.y = 39;
 		}
+	}
+	update(){
+		this.y += this.vy;
+		this.x += this.vx;
 		
+		this.checkLimits();	
+
+		// Draw this;
+		//drawSprite(subSprite[(frameCounter>>2)%3],this.x,this.y,this.dir,this.color,this.hScale);
+		drawSprite(subSprite[(this.animationCounter>>animationSpeed)%3],this.x,this.y,this.dir,this.color,this.hScale);
+		this.animationCounter++;
 	}
 	checkCollision(player){
 		return false;
 	}
+
 }
 
 
 
-
-
-
-// Converts a HEX RIA pallete code to RGB format.
-function tiaColor(value){
-	return("#" + ntscTiaPallete[Math.floor(value/2)].toString(16).padStart(6,"0"));
-}
-
-// This function emulates the exact behavior of Sea Quest shuffle routine.
+// This function emulates the exact behavior of Sea Quest shuffle routine at memory address 0xFEDB.
 function shuffle(value){
 /*  Original Sea Quest shuffle routine in 6502 assembly.
 
@@ -107,7 +89,8 @@ loop:
 	bpl	loop	;if last operation results positive goto loop.
 	rts
 */
-	
+
+	// The code here is not optimized just to show how the original Atari 2600 routine works.
 	var c_flag;			// Flag that indicates if last operation resulted in a
 						// overflow of 8bit 6502 register
 	for(var y_reg = 2 ; y_reg >= 0; y_reg--){ //y_reg equivalent to Y register
@@ -127,164 +110,6 @@ loop:
 	return(value & 0xFF); // returns just a 8bit value
 }
 
-// Sprites
-
-// Sub Sprite
-// Can you see a sub here ??
-const subSprite = [
-	[
-	0b00000100,
-	0b00001100,
-	0b00001100,
-	0b00001100,
-	0b00001100,
-	0b00111111,
-	0b11111111,
-	0b11111101,
-	0b01111111,
-	0b11111110,
-	0b10000000],
-	[
-	0b00000100,
-	0b00001100,
-	0b00001100,
-	0b00001100,
-	0b00001100,
-	0b10111111,
-	0b01111111,
-	0b11111101,
-	0b11111111,
-	0b01111110,
-	0b10000000],
-	[
-	0b00000100,
-	0b00001100,
-	0b00001100,
-	0b00001100,
-	0b00001100,
-	0b10111111,
-	0b11111111,
-	0b01111101,
-	0b11111111,
-	0b11111110,
-	0b00000000],
-];
-
-// Mini Sub ico
-lifeIco = [
-	0b00000100,
-	0b00001100,
-	0b00001100,
-	0b10111110,
-	0b11111110,
-	0b11111110,
-	0b11111100,
-	0b10000000
-];
-
-numberSprite = [
-	[// 0 
-	0b00111100,
-	0b01100110,
-	0b01100110,
-	0b01100110,
-	0b01100110,
-	0b01100110,
-	0b01100110,
-	0b00111100
-	],
-	[// 1 
-	0b00011000,
-	0b00111000,
-	0b00011000,
-	0b00011000,
-	0b00011000,
-	0b00011000,
-	0b00011000,
-	0b00111100
-	],
-	[// 2
-	0b00111100,
-	0b01000110,
-	0b00000110,
-	0b00000110,
-	0b00111100,
-	0b01100000,
-	0b01100000,
-	0b01111110
-	],
-	[// 3
-	0b00111100,
-	0b01000110,
-	0b00000110,
-	0b00001100,
-	0b00001100,
-	0b00000110,
-	0b01000110,
-	0b00111100,
-	],
-	[// 4
-	0b00001100,
-	0b00011100,
-	0b00101100,
-	0b01001100,
-	0b01111110,
-	0b00001100,
-	0b00001100,
-	0b00001100
-	],
-	[// 5
-	0b01111110,
-	0b01100000,
-	0b01100000,
-	0b01111100,
-	0b00000110,
-	0b00000110,
-	0b01000110,
-	0b01111100
-	],
-	[// 6
-	0b00111100,
-	0b01100010,
-	0b01100000,
-	0b01111100,
-	0b01100110,
-	0b01100110,
-	0b01100110,
-	0b00111100
-	],
-	[// 7
-	0b01111110,
-	0b01000010,
-	0b00000110,
-	0b00001100,
-	0b00011000,
-	0b00011000,
-	0b00011000,
-	0b00011000
-	],
-	[// 8
-	0b00111100,
-	0b01100110,
-	0b01100110,
-	0b00111100,
-	0b00111100,
-	0b01100110,
-	0b01100110,
-	0b00111100,
-	],
-	[// 9
-	0b00111100,
-	0b01100110,
-	0b01100110,
-	0b01100110,
-	0b00111110,
-	0b00000110,
-	0b01000110,
-	0b00111100
-	],
-
-];
 
 function drawSprite(sprite,x,y,dir,color,hScale){
 	// Actually X Canvas position based in Atari x position.
@@ -297,17 +122,22 @@ function drawSprite(sprite,x,y,dir,color,hScale){
 		// If sprite are in sea line discard 4 draw lines.
 		if(y + line < 46 || y + line > 49){
 			for(var i = 0; i<8;i++){
-				if(((sprite[line]<<i)&0x80)==0x80){
-					ctx.fillRect(x*2*canvasScale+2*i*hScale*canvasScale,y*canvasScale+line*canvasScale,2*hScale*canvasScale,canvasScale);
+				if(dir == 1){
+					if(((sprite[line]<<i)&0x80)==0x80){
+						ctx.fillRect(x*2*canvasScale+2*i*hScale*canvasScale,y*canvasScale+line*canvasScale,2*hScale*canvasScale,canvasScale);
+					}	
+				}
+				if(dir == -1){
+					if(((sprite[line]>>i)&0x01)==0x01){
+						ctx.fillRect(x*2*canvasScale+2*i*hScale*canvasScale,y*canvasScale+line*canvasScale,2*hScale*canvasScale,canvasScale);
+					}	
 				}
 			}
 		}
 	}
 }
 
-
-function drawBG(){
-	
+function drawBG(){	
 	/*if(seaTokenCounter == 7){
 		seaToken = shuffle(seaToken);
 		seaTokenCounter = 0;
@@ -332,9 +162,9 @@ function drawBG(){
 		[97,0x90],	// Deep Sea
 		[2,0xa0],
 		[2,0xb0],
-		[11,0xc0],	// Sea Botton
-		[7,0x32],
-		[13,0x06],
+		[4,0xc0],	// Sea Botton
+		//[7,0x32],
+		[27,0x06],
 		[12,0x00]]
 		
 	// Clear Screen
@@ -356,9 +186,8 @@ function drawBG(){
 	
 	// Draw playfield;
 	
-	
 	// Draw Player;
-	drawSprite(subSprite[(frameCounter>>2)%3],player.x,player.y,1,0x18,2);
+	//drawSprite(subSprite[(frameCounter>>2)%3],player.x,player.y,player.dir,0x18,2);
 	
 	
 	// Draw Score
@@ -376,10 +205,33 @@ function drawBG(){
 	for(i = 0; i< lifes; i++ ){
 		drawSprite(lifeIco,66-8 + i * 8,15,1,0x1A,1);
 	}
-	// 
+	
+	
+	// Draw seabed mountains
+	var montain = [ 3,2,1,0,0,1,2,3];
+	for(i = 0 ;i < 40; i++){
+		ctx.fillStyle = tiaColor(0xc0);
+		ctx.fillRect(i*8*canvasScale,154*canvasScale,8*canvasScale,canvasScale*montain[i%8]);
+	}
+	
+	
+	// Draw Oxygen Sprite
+	for(i = 0; i < 3; i++)
+		drawSprite(oxygenSprite[i], 15 + i*8 ,163,1,0x00,1);
+	// Draw Oxygen Bar
+	ctx.fillStyle = tiaColor(0x32);
+	ctx.fillRect(2*49*canvasScale,163*canvasScale,2* maxOxygenBar * canvasScale,5*canvasScale);
+	ctx.fillStyle = tiaColor(0x0C);
+	ctx.fillRect(2*49*canvasScale,163*canvasScale,2* oxygen * canvasScale,5*canvasScale);
+	
+	// Draw rescued divers
+	for(i = 0; i< lifes; i++ ){
+		drawSprite(diverIco,58 + i * 8,170,1,0x84,1);
+	}
+	
+	// Draw left black offset
 	ctx.fillStyle = tiaColor(0x00);
 	ctx.fillRect(0,0,16*canvasScale,height);
-	
 	
 	// Update the frame counter.
 	frameCounter++;
@@ -396,20 +248,31 @@ function frameDraw(){
 
 function gameLogic(){
 	player.update();
+	if(player.y == 39 && oxygen < maxOxygenBar && (frameCounter&1) == 1 )
+		oxygen++;
+	if(player.y > 45 && oxygen > 0 && (frameCounter&0b00011111) == 0b00011111 ){
+		oxygen--;	
+	}
+	//if sub semi suberged oxygen =0;
+	//each 32 frame oxygen--
 	
 }
 
 function frameLoop(){
-	gameLogic();
-	frameDraw();
 	
+	frameDraw();
+	gameLogic();
 }
 
 function playerMove(e){
-	
+	//issue : if left press on right pressed , release stops move , same as up down
 	var code = e.keyCode;
+	//console.log(code);
 	if(e.type == "keydown")
 		switch (code) {
+			case 32:
+				//player.fire()
+				break;
 			case 37: 
 				player.vx = -1; 
 				player.dir = -1;
@@ -440,15 +303,18 @@ function init(){
 	seaToken = 0x08;
 	// Counter used to generate a new seaToken.
 	seaTokenCounter = 0;
-	score = 20;
+	score = 0;
 	lifes = 3;
+	oxygen = 0;
+	rescuedDivers = 3;
 	
 	// Frame counter for use in animations and miscs.
 	frameCounter = 0;
 	
-	player = new Enemy(subSprite,x,y,0x18,2);
+	
 	// Set refresh to 60, like the original Atari 2600 hardware.
 	updateTimerTimerId = setInterval(frameLoop, 16);
+	player = new Enemy(subSprite,76,39,0x18,2);
 	//frameLoop();
 	
 	window.addEventListener('keydown',playerMove,false);
