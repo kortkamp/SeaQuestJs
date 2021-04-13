b8e9411950d036f53e796d83ba745ef0dafa3e6c
kortkamp <marcelusmedius@gmail.com>
2021-04-13 08:56:46 -0300

    add mobile controls

--- game.js
+++ game.js
@@ -267,9 +267,10 @@ class Diver extends GameObject{
 }
 
 class Player extends GameObject{
-	constructor(){
+	constructor(playerInput){
 		
 		super(subSprite,startPlayerPosition.x,startPlayerPosition.y,0x18,2);
+		this.input = playerInput;
 		this.baseSpeed = 1;
 		this.hScale = 2;
 		this.oxygen = 0;
@@ -279,6 +280,26 @@ class Player extends GameObject{
 		this.subStillSurfaced = true;
 		this.explosionFrameCounter = 0;
 		this.explosionInAction = false;
+	}
+	update(){
+		this.y += this.input.getY();
+		this.vx = this.input.getX();
+		this.x += this.vx;
+		if(this.vx > 0)
+			this.dir = 1;
+		if(this.vx < 0)
+			this.dir = -1;
+		if(this.input.getFire())
+			torpedo.fire();
+		//if(this.input.getFire())
+		//	this.fire();
+		
+		this.checkLimits();	
+		this.checkInternals();
+		
+		var spriteNumber = (this.animationCounter>>this.animationSpeed)%3;
+		drawSprite(this.sprite[spriteNumber],Math.floor(this.x),Math.floor(this.y),this.dir,this.color,this.hScale);
+		this.animationCounter++;
 	}
 	// Player must not pass these limits, ask Activion about this.
 	checkLimits(){
@@ -725,39 +746,111 @@ function frameLoop(){
 	ctx.fillRect(0,0,8,height);
 }
 
-function playerMove(e){
-	//issue : if left press on right pressed , release stops move , same as up down
-	var code = e.keyCode;
-	//console.log(code);
-	if(e.type == "keydown")
-		if(!player.engineStoped){
-			switch (code) {
-				case 32:
-					torpedo.fire(player);
-					break;
-				case 37: 
-					player.vx = -player.baseSpeed; 
-					player.dir = -1;
-					break; //Left key
-				
-				case 38: player.vy = -player.baseSpeed; break; //Up key
-				case 39: 
-					player.vx = player.baseSpeed; 
-					player.dir = 1;
-					break; //Right key
-				case 40: player.vy = player.baseSpeed; break; //Down key     
+
+class Input{
+	constructor(){
+		this.moveLeft = 0;
+		this.moveRight = 0;
+		this.moveUp = 0;
+		this.moveDown = 0;
+		this.fire = false;
+		this.stick = null;
+		this.stickArea = document.getElementById("stickBorder"); 
+		this.controller = document.getElementById("controller"); 
+		this.controllerX = 0;
+		this.controllerY = 0;
+		this.controllerCenterX = this.stickArea.offsetLeft+this.stickArea.offsetWidth/2;
+		this.controllerCenterY = this.stickArea.offsetTop+this.stickArea.offsetHeight/2+ this.controller.offsetTop;
+	}
+	keyboardListener(e){
+		
+		var code = e.keyCode;
+		if(e.type == "keydown"){
+			if(!player.engineStoped){
+				switch (code) {
+					case 32: this.fire = true; break; // Space
+					case 37: this.moveLeft = 1; break; //Left key
+					case 38: this.moveUp = 1; break; //Up key
+					case 39: this.moveRight = 1; break; //Right key
+					case 40: this.moveDown = 1;	break; //Down key     
+				}
 			}
 		}
-	if(e.type == "keyup")
-		switch (code) {
-			case 37: player.vx = 0; break; //Left key
-			case 38: player.vy = 0; break; //Up key
-			case 39: player.vx = 0; break; //Right key
-			case 40: player.vy = 0; break; //Down key     
+		if(e.type == "keyup"){
+			
+			switch (code) {
+				case 32: this.fire = false; break; //space
+				case 37: this.moveLeft = 0; break; //Left key
+				case 38: this.moveUp = 0; break; //Up key
+				case 39: this.moveRight = 0; break; //Right key
+				case 40: this.moveDown = 0; break; //Down key     
+			}
+			
 		}
+		
+	}
+	cotrollerListener(e,input){
+		
+
+		if(e.type === "touchend"){
+			input.controllerX = 0;
+			input.controllerY = 0;
+			//release controls
+			//console.log(e.type);
+		}else if(e.type === "touchstart" || e.type === "touchmove"){
+			var x = e.touches[0].clientX - input.controllerCenterX;
+			var y = e.touches[0].clientY - input.controllerCenterY;
+			var direction  = Math.PI - Math.atan2(x,y);
+			
+			var threshold = 100;
+			if(x > threshold) 
+				input.controllerX = 1;
+			if(x < -threshold) 
+				input.controllerX = -1;
+			if(y > threshold) 
+				input.controllerY = 1;
+			if(y < -threshold) 
+				input.controllerY = -1;
+
+			//console.log("coords: " + input.controllerX + "," + input.controllerY);	
+			//if(direction)
+			//console.log((e.touches[0].clientX - input.controllerCenterX));
+			//console.log((e.touches[0].clientY - input.controllerCenterY));
+		}
+
+	}
+	initControllerListener(input){
+		
+		
+		// Add listener functions to mouse events.
+		this.stickArea.addEventListener("mousedown", this.cotrollerListener, false);
+		this.stickArea.addEventListener("mouseup", this.cotrollerListener, false);
+		this.stickArea.addEventListener("mousemove", this.cotrollerListener, false);
+
+		// Add listener functions to touch events.
+		this.stickArea.addEventListener("touchstart", function(e){input.cotrollerListener(e,input)}, false);
+		this.stickArea.addEventListener("touchend", function(e){input.cotrollerListener(e,input)}, false);
+		this.stickArea.addEventListener("touchmove",function(e){input.cotrollerListener(e,input)}, false);
+	}
+	getControllerX(){
+
+	}
+	getControllerY(){
+
+	}
+	getControllerFire(){
+
+	}
+	getX(){
+		return(this.moveRight-this.moveLeft + this.controllerX);
+	}
+	getY(){
+		return(this.moveDown-this.moveUp + this.controllerY);
+	}
+	getFire(){
+		return(this.fire);
+	}
 }
-
-
 
 function resetGame(){
 	
@@ -792,8 +885,24 @@ function loadSounds(){
 
 }
 
+function mobileCheck() {
+	let check = false;
+	(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+	return check;
+  };
+
 function init(){
 	
+	if(mobileCheck()){
+		console.log("mobile");
+		document.getElementById("gameCanvas").style.width = window.innerWidth + "px";
+		document.getElementById("gameCanvas").style.height = Math.floor(window.innerWidth/1.66) + "px";
+		document.getElementById("controller").style.display = "inline-block";
+	}else{
+		document.getElementById("gameCanvas").style.width = "70vw";
+		document.getElementById("gameCanvas").style.height = "42vw";
+	}
+
 	width = cv.width
 	height = cv.height
 
@@ -801,11 +910,14 @@ function init(){
 	
 	// Set refresh to 60, like the original Atari 2600 hardware.
 	updateTimerTimerId = setInterval(frameLoop, 16);
-	player = new Player();
+	input = new Input();
+	input.initControllerListener(input);
+	player = new Player(input);
 	torpedo = new Torpedo(player,0,0,1);
 		
-	window.addEventListener('keydown',playerMove,false);
-	window.addEventListener('keyup',playerMove,false);	
+	
+	window.addEventListener('keydown',function(e){input.keyboardListener(e)},false);
+	window.addEventListener('keyup',function(e){input.keyboardListener(e)},false);	
 	resetGame();
 	//frameLoop();
 	
